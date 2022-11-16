155860a5b93acc1e994d86edb6f3b46018a64628
Antoine Berson <antoine.berson01@gmail.com>
2022-11-16 22:58:55 +0100

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants..."
-dishoom = {name: 'Dishoom', address: '7 Boundary St, London E2 7JE', stars: 5}
-pizza_east =  {name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', stars: 4}
-petite_brasserie  =  {name: 'Petite Brasserie', address: '5 Pl. Saint-Pierre, 33000 Bordeaux', stars: 4}
-petit_bois =  {name: 'Petit Bois', address: '12 Pl. du Parlement, 33000 Bordeaux', stars: 4}
-aperitivo  =  {name: ' Aperitivo ', address: '12 Pl. du Parlement, 33000 Bordeaux', stars: 4}
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'chinese' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian' }
+petite_brasserie = { name: 'Petite Brasserie', address: '5 Pl. Saint-Pierre, 33000 Bordeaux', category: 'french' }
+petit_bois =  { name: 'Petit Bois', address: '12 Pl. du Parlement, 33000 Bordeaux', category: 'belgian' }
+aperitivo  =  { name: ' Aperitivo ', address: '15 Pl. du Parlement, 33000 Bordeaux', category: 'japanese' }
 
 [dishoom, pizza_east, petite_brasserie, petit_bois, aperitivo].each do |attributes|
   restaurant = Restaurant.create!(attributes)
